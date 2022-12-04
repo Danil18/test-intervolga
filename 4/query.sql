a7febf56ac15126a5d795e8ad128ebf3c17190f0
Danil18 <36386580+Danil18@users.noreply.github.com>
2022-12-05 01:31:11 +0300

    Update query.sql

--- 4/query.sql
+++ 4/query.sql
@@ -1,3 +1,3 @@
-SELECT full_name FROM sportsman JOIN results ON id = results.id_sportsman
+SELECT full_name FROM sportsman JOIN results ON sportsman.id = results.id_sportsman
 GROUP BY full_name ORDER BY COUNT(results.id_sportsman) DESC
 LIMIT 5;
